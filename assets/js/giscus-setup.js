4a49873c824164111b2b97379c903927191aa1c3
tmorstyn <44834416+tmorstyn@users.noreply.github.com>
2026-04-19 20:52:26 +0000

    Deploying to gh-pages from @ PSALOxford/psaloxford.github.io@b1ac9b05abdec5984a5d95d2e477f8bcde5e2fee 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "PSALOxford/website-al-folio",
+    "data-repo": "PSALOxford/psaloxford.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
